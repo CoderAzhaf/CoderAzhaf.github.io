c5a9e1ea07a2509a9cf8bc7a879217215e720e76
CoderAzhaf <168289058+CoderAzhaf@users.noreply.github.com>
2024-05-28 20:46:25 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function alertbutton() {
-    alert("Welcome to the AZHAFUDDiN MOHAMMED website!")
+    alert("Welcome to the AZHAFUDDiN MOHAMMED website! have lots of fun!!!!!!!!!!!!!")
 }
 let username;
 username = window.prompt("What do you want to say before you want to enter the website?");
