d2c5e93e786fb8b297180d015d0b4a0f1b93a7bf
CoderAzhaf <168289058+CoderAzhaf@users.noreply.github.com>
2025-02-16 09:11:34 -0600

    app.js

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ function alertbutton() {
     alert("Welcome to the AZHAFUDDiN MOHAMMED website! have lots of fun!!!!!!!!!!!!!")
 }
 let username;
-username = window.prompt("What do you want to say before you want to enter the website?");
+username = window.prompt("Welcome to the website type something!");
