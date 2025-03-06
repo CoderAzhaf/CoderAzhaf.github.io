79ffe61221320299924ce10fd3b1c3eac01d7a70
Coder Azhaf <168289058+CoderAzhaf@users.noreply.github.com>
2025-03-05 20:10:05 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -2,4 +2,5 @@ function alertbutton() {
     alert("Welcome to the AZHAFUDDiN MOHAMMED website! have lots of fun!!!!!!!!!!!!!")
 }
 let username;
-username = window.prompt("Welcome to the website type something!");
+username = window.prompt("Welcome to the website type something!")
+
