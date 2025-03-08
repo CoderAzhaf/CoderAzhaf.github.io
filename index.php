d89e4a4602ec9b76a93d2f2b8469117a0e83f737
Coder Azhaf <168289058+CoderAzhaf@users.noreply.github.com>
2025-03-08 06:39:47 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,22 +1,19 @@
 <?php
 // filepath: c:\Users\mjama\OneDrive\Desktop\Azhaf codez\index.php
 <?php
-$blocked_ips = array('67.204.23.234', '67.204.23.234'); // Add the IP addresses you want to block
+$blocked_ips = array('
+45.22.61.221', '
+45.22.61.221'); // Add the IP addresses you want to block
 
 if (in_array($_SERVER['REMOTE_ADDR'], $blocked_ips)) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this page.');
 }
 ?>
-
-
- {
+# filepath: /etc/nginx/nginx.conf
+server {
     ...
-    deny # filepath: /etc/nginx/nginx.conf
-    server {
-        ...
-        deny 67.204.23.234;
-        ...
-    };
+    deny 
+    45.22.61.221;
     ...
 }
