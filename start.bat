cd344e1dbf8b15c58d9e8b0da72abc778e58d8cf
Coder Azhaf <168289058+CoderAzhaf@users.noreply.github.com>
2025-05-31 16:52:21 -0500

    Add files via upload

--- start.bat
+++ start.bat
@@ -1,2 +1,12 @@
 npm install
-npm start
+npm start 
+
+@echo off
+REM Install dependencies
+call npm install
+
+REM Start the server
+call npm start
+
+REM Keep window open if there's an error
+pause
